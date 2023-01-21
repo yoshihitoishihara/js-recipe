a87a77884bd30a14bc67ebb1401b34958ca24345
yoshihitoishihara <yoshihito2103@outlook.jp>
2023-01-21 16:26:22 +0900

    [Trello (保存機能なし)]を学習しました。

---
+++ 02_レシピ/02_Trello（保存機能なし）/main.js
@@ -0,0 +1,29 @@
+const inputElement = document.getElementById("input-todo")
+const container = document.getElementById("cards-container")
+const addButton = document.getElementById("add-button")
+
+addButton.onclick = function() {
+    const card = createCard(inputElement.value)
+    container.append(card)
+
+    inputElement.value = ""
+}
+
+const createCard = function(text) {
+    const card = document.createElement("div")
+    card.className = "card"
+
+    const todo = document.createElement("div")
+    todo.className = "todo"
+    todo.textContent = text
+    card.append(todo)
+    
+    const deleteButton = document.createElement("div")
+    deleteButton.className = "delete"
+
+    deleteButton.onclick = function() {
+        card.remove()
+    }
+    card.append(deleteButton)
+    return card
+}
